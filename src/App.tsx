671a1bd693ca16f37f82ce5f1b1c49262cd8fdeb
shivhendo <shivhendo@proton.me>
2022-09-02 12:25:23 -0400

    init react-babylon playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 
-function App() {
+import { Camera } from "@babylonjs/core";
+import { Color3 } from "@babylonjs/core/Maths/math.color";
+import { Mesh } from "@babylonjs/core/Meshes/mesh";
+import { Nullable } from '@babylonjs/core/types';
+import { Texture } from "@babylonjs/core/Materials/Textures/texture";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+
+import { Scene, Engine } from 'react-babylonjs';
+
+import "./App.css";
+
+const App: React.FC = () => {
+  //let sphereRef = useRef(null);
+  const sphereRef = useRef<Nullable<Mesh>>(null);
+
+  const [camera, setCamera] = useState<Camera>();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Engine antialias adaptToDeviceRatio canvasId="babylonJs">
+          <Scene>
+            <arcRotateCamera
+              name="camera1"
+              target={Vector3.Zero()}
+              alpha={Math.PI / 2}
+              beta={Math.PI / 4}
+              radius={4}
+              onCreated={camera => setCamera(camera)}
+            />
+            <hemisphericLight name='hemi' direction={Vector3.Up()} intensity={0.8} />
+            <sphere ref={sphereRef} name="sphere1" diameter={5} segments={16} position={new Vector3(0, 5, 0)}>
+              <standardMaterial name='material1' specularPower={16}
+                  diffuseColor={Color3.Black()}
+                  emissiveColor={new Color3(0.5, 0.5, 0.5)}
+              />
+            </sphere>
+          </Scene>
+        </Engine>
       </header>
     </div>
   );
