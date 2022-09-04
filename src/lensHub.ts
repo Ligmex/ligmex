99b8fca6f32dc22a7a08bf4791fa569ebf41a7e6
bohendo <bohendo@pm.me>
2022-09-03 22:35:14 -0400

    fix abis

--- src/lensHub.ts
+++ src/lensHub.ts
@@ -1,19 +1,22 @@
 import { ethers } from 'ethers';
 
-const LENS_HUB_ABI = "./abis/lens-hub-contract-abi";
-const LENS_PERIPHERY_ABI = "./abis/lens-periphery-data-provider";
+import LENS_HUB_ABI from "./abis/lens-hub-contract-abi.json";
+import LENS_PERIPHERY_ABI from "./abis/lens-periphery-data-provider.json";
+
 const MUMBAI_RPC_URL = "https://rpc-mumbai.matic.today";
 const provider = new ethers.providers.JsonRpcProvider(MUMBAI_RPC_URL);
+
+console.log(`LENS_HUB_ABI: ${JSON.stringify(LENS_HUB_ABI)}`)
 
 export const LENS_HUB_CONTRACT = "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82";
 export const LENS_PERIPHERY_CONTRACT = "0xD5037d72877808cdE7F669563e9389930AF404E8";
 
 // lens contract info can all be found on the deployed
 // contract address on polygon.
-export const lensHub = new ethers.Contract(LENS_HUB_CONTRACT, LENS_HUB_ABI, provider );
+export const lensHub = new ethers.Contract(LENS_HUB_CONTRACT, LENS_HUB_ABI.abi, provider );
 
 export const lensPeriphery = new ethers.Contract(
   LENS_PERIPHERY_CONTRACT,
-  LENS_PERIPHERY_ABI,
+  LENS_PERIPHERY_ABI.abi,
   provider
 );
