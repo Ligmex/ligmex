f7c38b0f2a3babad9ea6578f98e7e754f1d1d80d
bohendo <bohendo@pm.me>
2022-09-24 13:57:59 -0400

    add ctrl pannel camera constant

--- modules/client/src/components/Home.tsx
+++ modules/client/src/components/Home.tsx
@@ -31,7 +31,7 @@ import {
 } from "../utils";
 import {
   PROFILE_FRAME_POSITION,
-  PROFILE_FRAME_VIEW_POSITION,
+  CTRL_PANEL_VIEW_POSITION,
   PROFILE_FRAME_VIEW_ROTATION,
   TRENDING_CORNER_POSITION,
   TRENDING_VIEW_POSITION,
@@ -53,7 +53,7 @@ export const Home = () => {
   const [sceneState, setSceneState] = useState({
     profileToLoad: storedProfile ? storedProfile : "",
     camera: storedProfile? {
-      position: PROFILE_FRAME_VIEW_POSITION,
+      position: CTRL_PANEL_VIEW_POSITION,
       rotation: PROFILE_FRAME_VIEW_ROTATION
     } : {
       position: TRENDING_VIEW_POSITION,
--- modules/client/src/utils/cameraConstants.ts
+++ modules/client/src/utils/cameraConstants.ts
@@ -1,5 +1,6 @@
 import { Vector3 } from "@babylonjs/core";
 
+export const CTRL_PANEL_VIEW_POSITION = new Vector3(10, 2, 4);
 export const PROFILE_FRAME_VIEW_POSITION = new Vector3(-8, 2, -6);
 export const PROFILE_FRAME_POSITION = new Vector3(-10, 0, 10);
 export const PROFILE_FRAME_VIEW_ROTATION = new Vector3(0, 0, 0);
